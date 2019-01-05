da271140ca911bceb54937cda7767e71d43df4e5
Murph Grainger <murph.grainger@gmail.com>
2019-01-05 13:51:55 -0700

    added staging db to knex

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,6 +6,11 @@ module.exports = {
     connection: process.env.DEV_DB_URL
   },
 
+  staging: {
+    client: 'pg',
+    connection: process.env.STAGING_DB_URL + '?ssl=true'
+  },
+
   production: {
     client: 'pg',
     connection: process.env.PRODUCTION_DB_URL + '?ssl=true'
