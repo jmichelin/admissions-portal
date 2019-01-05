371ceb0f3050ff495d74166899adc34e7f666cd3
Murph Grainger <murph.grainger@gmail.com>
2019-01-05 15:16:39 -0700

    added parth module

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
