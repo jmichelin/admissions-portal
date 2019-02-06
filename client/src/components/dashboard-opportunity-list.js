150adda4586089281503bbc79d0621ffe884e963
Murph Grainger <murph.grainger@gmail.com>
2019-02-06 14:14:52 -0700

    removing table columns for mobile

--- client/src/components/dashboard-opportunity-list.js
+++ client/src/components/dashboard-opportunity-list.js
@@ -22,9 +22,9 @@ class OpportunityList extends Component {
         <div className="application-row" key={i}>
           <ul className="table-row -listing">
             <li>{course}</li>
-            <li>{campus}</li>
-            <li>{moment(opp.courseStart).format('MM/DD/YYYY')}</li>
-            <li>{opp.currentStep.status}</li>
+            <li className="hide-mobile">{campus}</li>
+            <li className="hide-mobile">{moment(opp.courseStart).format('MM/DD/YYYY')}</li>
+            <li className="hide-tablet">{opp.currentStep.status}</li>
           </ul>
           <div className="table-row -steps">
             <AdmissionsProcessList program={opp.courseProduct} currentStep={opp.currentStep} opp={opp}/>
@@ -39,9 +39,9 @@ class OpportunityList extends Component {
         <div className="table-head">
           <ul className="table-row">
             <li>Your Active Applications</li>
-            <li>Campus</li>
-            <li>Start Date</li>
-            <li>Next Step</li>
+            <li className="hide-mobile">Campus</li>
+            <li className="hide-mobile">Start Date</li>
+            <li className="hide-tablet">Next Step</li>
           </ul>
         </div>
         <div className="table-body">
