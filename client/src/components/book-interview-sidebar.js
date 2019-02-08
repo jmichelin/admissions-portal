0f46c084416023eb651799899de2793e288bc6be
Murph Grainger <murph.grainger@gmail.com>
2019-02-08 14:49:45 -0700

    updated content

--- client/src/components/book-interview-sidebar.js
+++ client/src/components/book-interview-sidebar.js
@@ -7,13 +7,13 @@ export default (props) => {
     <h4 className="column-headline">Things to Know</h4>
     <ul>
       <li>The interview will be <span>60 minutes long.</span></li>
-      <li>To ensure you can interview in time for your desired cohort, you should book an interview and start preparing immediately, as interview sports fill up quickly.  If a location's calendar is full, try booking on one of the other ones (acceptance applies network wide).</li>
-      <li>A max of 3 interview attempts is permitted network-wide.</li>
+      <li>To ensure you can interview in time for your desired cohort, you should book an interview and start preparing immediately, as interview sports fill up quickly.  If a location's calendar is full, try booking on one of the other ones (acceptance applies network-wide).</li>
+      <li>A max of three interview attempts is permitted network-wide.</li>
     </ul>
 
     <div className="card-help">
-      <h4>Got Questions?</h4>
-      <p>We have the answers to help you prepare for the technical interview.</p>
+      <h4>Have Questions?</h4>
+      <p>We have the answers to help you prepare for the Technical Interview.</p>
       <a className="button-secondary" href="https://www.galvanize.com/web-development/interview" rel="noopener noreferrer" target="_blank">About the Tech Interview</a>
       <h5>Contact</h5>
       <a href="mailto:admissions@galvanize.com">admissions@galvanize.com</a>
--- client/src/helpers/utils.js
+++ client/src/helpers/utils.js
@@ -28,16 +28,16 @@
     if (opp.scorecard.moveForwardCode !== 'Yes') {
       //person needs to do coding challenge
       return SEI_STEPS.STEP_TWO;
-    } else if (opp.scorecard.moveForwardCode === 'Yes' && opp.scorecard.moveForwardInterview !== 'No' && opp.scorecard.moveForwardInterview !== 'Yes' && opp.stage !== 'Interview 1 Scheduled') {
+    } else if (opp.scorecard.finalCode && opp.scorecard.moveForwardCode === 'Yes' && opp.scorecard.moveForwardInterview !== 'No' && opp.scorecard.moveForwardInterview !== 'Yes' && opp.stage !== 'Interview 1 Scheduled') {
       //passed coding challenge but person needs to book the interview
       return SEI_STEPS.STEP_THREE;
-    } else if (opp.scorecard.moveForwardCode === 'Yes' && opp.stage === 'Interview 1 Scheduled') {
+    } else if (opp.scorecard.finalCode && opp.scorecard.moveForwardCode === 'Yes' && opp.stage === 'Interview 1 Scheduled') {
       //passed coding challenge and booked interview
       return SEI_STEPS.STEP_FOUR;
-    } else if (opp.scorecard.moveForwardCode === 'Yes' && opp.scorecard.moveForwardInterview === 'No') {
+    } else if (opp.scorecard.finalCode && opp.scorecard.moveForwardCode === 'Yes' && opp.scorecard.moveForwardInterview === 'No') {
       //passed coding challenge and booked interview but failed
       return SEI_STEPS.HOLD;
-    } else if (opp.scorecard.moveForwardCode === 'Yes' && opp.scorecard.moveForwardInterview === 'Yes') {
+    } else if (opp.scorecard.finalCode && opp.scorecard.moveForwardCode === 'Yes' && opp.scorecard.moveForwardInterview === 'Yes') {
       //passed coding challenge and booked interview and passed
       return SEI_STEPS.COMPLETE;
     } else {
