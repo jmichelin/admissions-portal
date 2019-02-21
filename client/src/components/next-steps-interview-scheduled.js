14c0ccfb64f901978edf7827f8f17d913d0032df
Murph Grainger <murph.grainger@gmail.com>
2019-02-20 18:02:48 -0700

    updated text syntax

--- client/src/components/next-steps-interview-scheduled.js
+++ client/src/components/next-steps-interview-scheduled.js
@@ -5,13 +5,14 @@ export default (props) => {
 
   return (
     <div className="next-steps -gray">
-      <div className="">
+      <div className="left-text">
         <h4>Next Steps</h4>
         <p className="-inverse">Your interview is booked! Prepare to pass your Technical Interview by enrolling in a Galvanize Software Engineering Prep course or by practicing the JavaScript fundamentals on your own.</p>
-        <p className="-inverse">Need to reschedule or cancel your interview?  Refer to your booking confirmation email. <br></br><br></br><span className="-alert">NOTE: If you booked your interview within the Hack Reactor Admissions Portal and are trying to reschedule please <Link className="inverse" to={{
+        <p className="-inverse">Need to reschedule or cancel your interview?  Refer to your booking confirmation email.</p>
+          <p className="-alert -inverse">NOTE: If you booked your interview within the previous Hack Reactor Admissions Portal and are trying to reschedule please <Link className="inverse" to={{
                     pathname: "/book-interview",
                     state: { opp: props.opp, override: true} }}>
-                    click here</Link> to rebook. We will handle cancelling your old booking on our end.</span></p>
+                    click here</Link> to rebook. We will handle cancelling your old booking on our end.</p>
       </div>
     </div>
   )
