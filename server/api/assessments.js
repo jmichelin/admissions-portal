36adc155eca8e5b6798cf1a03614add97ee94afe
Murph Grainger <murph.grainger@gmail.com>
2019-04-18 09:41:10 -0600

    working on endpoint

--- server/api/assessments.js
+++ server/api/assessments.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 const Q = require('../db/queries');
 
+import { SNIPPET_1 } from '../constants';
 import Assessments from '../lib/assessments';
 
+const assessmentz = new Assessments();
 
 router.post('/', (req, res, next) => {
  let assessment = {
@@ -16,11 +18,27 @@ router.post('/', (req, res, next) => {
  }
  Q.addNewAssessment(assessment)
     .then(result => {
-      console.log(result);
+      let assessmentsPayload = {
+        code_to_assess: assessment.answer,
+        setup_to_run_before_code: '',
+        tests_to_assess_against: SNIPPET_1.tests,
+        language: 'python3.6',
+        callback_url: `${process.env.BASE_URL}/api/vi/assessments/webhook/${result[0].id}`
+      };
+
+     assessmentz.post(assessmentsPayload)
+      .then(thing => {
+        console.log('thing', thing);
+        res.send(200).end();
+      })
     })
 
 
 
+});
+
+router.patch('/webhook/:id', (req, res, next) => {
+  console.log('****', req.body);
 });
 
 
--- server/lib/assessments.js
+++ server/lib/assessments.js
@@ -1,4 +1,19 @@
+const fetch = require('node-fetch');
 
 class Assessments {
 
+ post(data) {
+   fetch(process.env.ASSESSMENTS_SERVICE_DOMAIN + '/assessments', {
+     method: 'POST',
+     body: JSON.stringify(data),
+     headers: {
+       Authorization: `Token token="${process.env.ASSESSMENTS_API_KEY}"`,
+       'Content-Type': 'application/json'
+     },
+   }).then(res => {
+     console.log(res.headers.raw());
+   })
+ }
 }
+
+export default Assessments;
