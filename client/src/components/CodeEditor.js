8e75d8e3a01cc7d57ef19c0a316c05e6ebf07e16
Chad Lillquist <chad.lillquist@galvanize.com>
2019-04-18 15:02:17 -0600

    first swipe at the react compononent for python challenges

--- client/src/components/CodeEditor.js
+++ client/src/components/CodeEditor.js
@@ -3,6 +3,7 @@ import {Controlled as CodeMirror} from 'react-codemirror2';
 
 import 'codemirror/lib/codemirror.css';
 require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/python/python');
 
 
 class CodeEditor extends Component {
@@ -12,34 +13,32 @@ class CodeEditor extends Component {
     this.state = {
       code: '// Enter your code here',
       cursorPos: { line: 0, column: 0 }
-      }
     }
+  }
 
   render() {
     const options = {
-      mode: 'javascript',
+      mode: this.props.mode,
       lineNumbers: true,
       lineWrapping: true,
       showCursorWhenSelecting: true,
       styleActiveLine: true
     }
 
-
-
-  return (
-    <div className="editor-wrapper">
-      <CodeMirror
-       editorDidMount={editor => { this.instance = editor }}
-       value={this.state.code}
-       options={options}
-       cursor={this.state.cursorPos}
-       onBeforeChange={(editor, data, code) => {this.setState({code})}}/>
-       <div className="action">
-         <span>{this.props.errorMessage}</span>
-         <button className={this.props.showProcessing ? "button-primary -loading" : "button-primary"} onClick={ (e) => this.props.codeTest(this.state.code, e) }>Test Code</button>
-         <button className={this.props.submittingCode ? "button-primary -loading" : "button-secondary"} disabled={!this.props.allPassed} onClick={ (e) => this.props.codeSubmit(e) }>Submit Code</button>
+    return (
+      <div className="editor-wrapper">
+        <CodeMirror
+        editorDidMount={editor => { this.instance = editor }}
+        value={this.state.code}
+        options={options}
+        cursor={this.state.cursorPos}
+        onBeforeChange={(editor, data, code) => {this.setState({code})}}/>
+        <div className="action">
+          <span>{this.props.errorMessage}</span>
+          <button className={this.props.showProcessing ? "button-primary -loading" : "button-primary"} onClick={ (e) => this.props.codeTest(this.state.code, e) }>Test Code</button>
+          <button className={this.props.submittingCode ? "button-primary -loading" : "button-secondary"} disabled={!this.props.allPassed} onClick={ (e) => this.props.codeSubmit(e) }>Submit Code</button>
+        </div>
       </div>
-    </div>
     )
   }
 }
