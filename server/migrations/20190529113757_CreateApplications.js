62e7c16870cd4f3db57faa3e0a52b1a77dbdf767
Murph Grainger <murph.grainger@gmail.com>
2019-05-30 17:09:58 -0600

    continued refactor of process steps

--- server/migrations/20190529113757_CreateApplications.js
+++ server/migrations/20190529113757_CreateApplications.js
@@ -2,7 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('application', function(table) {
     table.increments();
-    table.text('program').notNullable();
+    table.text('course_product').notNullable();
+    table.text('course_type').notNullable();
     table.jsonb('values').notNullable().defaultTo('{}');
     table.timestamp('complete');
     table.integer('user_id').unsigned().references('user.id').onDelete('CASCADE').notNullable();
