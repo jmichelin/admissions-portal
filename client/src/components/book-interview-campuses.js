8cc8e5317116a878c0ab636a46dcced5d35f2c6a
Murph Grainger <murph.grainger@gmail.com>
2019-09-25 10:54:08 -0600

    changed all interview text to be assessment

--- client/src/components/book-interview-campuses.js
+++ client/src/components/book-interview-campuses.js
@@ -8,17 +8,17 @@ export default (props) => {
     return (
       <div className="campus-card" key={i}>
         <h4>{campus.city}</h4>
-        <button className="button-tertiary" onClick={() => {props.loadBookingTool(campus)}}>Book Interview</button>
+        <button className="button-tertiary" onClick={() => {props.loadBookingTool(campus)}}>Book Assessment</button>
       </div>
     )
   })
 
   return (
     <div className="grouping">
-      <h4 className="column-headline">Select an Interview Location</h4>
-      { props.holdText && <p className="-center -bold">Be sure to book your next assessment at least two weeks after your previous attempt.</p> }
+      <h4 className="column-headline">Select a Calendar</h4>
       <p className="-center">All campuses share the same assessment format and rubric, so you can select any location that has availability that's most convenient for you, regardless of your preferred campus.</p>
-      <div className="card-holder">
+      { props.holdText && <p className="-center -bold">Be sure to book your next assessment at least two weeks after your previous attempt.</p> }
+    <div className="card-holder">
         {campusCards}
       </div>
     </div>
