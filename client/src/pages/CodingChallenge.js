3a03ecbd58519e2a3a69a2fb09c5cc3c6434908e
Murph Grainger <murph.grainger@gmail.com>
2019-10-09 10:58:47 -0600

    removed conosole logs

--- client/src/pages/CodingChallenge.js
+++ client/src/pages/CodingChallenge.js
@@ -111,7 +111,8 @@ class CodingChallenge extends Component {
         code: this.state.submittedCode,
         oppId: this.state.opp.id,
         moveForward: 'No',
-        stage: 'Sent Takehome'
+        stage: 'Sent Takehome',
+        key: 'Passed_JavaScript_Challenge__c'
       }
 
       fetch(CODE_CHALLENGE_ENDPOINT, {
