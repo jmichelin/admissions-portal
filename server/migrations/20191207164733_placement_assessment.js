cc503e9a22a85596f890f22ad7630c6f9c9fe51c
JohnMichelin <jmichelinphone@gmail.com>
2019-12-08 15:09:59 -0800

    feat(migration): adjust basic schema

--- server/migrations/20191207164733_placement_assessment.js
+++ server/migrations/20191207164733_placement_assessment.js
@@ -1,10 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('placement_assessment', function(table) {
     table.increments();
-    table.uuid('uuid').notNullable
-    table.json('content').notNullable()
-    table.integer('difficulty_rank').notNullable();
-    table.text('status').notNullable();
+    table.json('content').notNullable();
+    table.integer('difficulty_rank');
+    table.text('status').defaultTo('unsorted').notNullable();
   });
 };
 
